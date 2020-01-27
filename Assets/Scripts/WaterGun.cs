92447bab1444a9c57b16e63d362b7918e781e826
Gabriel-PC\Gabriel <gabrielbarbosa@msn.com>
2020-01-27 10:32:32 -0300

    Water gun
    Water gun

---
+++ Assets/Scripts/WaterGun.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.VFX;
+
+public class WaterGun : MonoBehaviour
+{
+
+    public VisualEffect vfx;
+    public float inputForce = 1;
+
+    private float multiplier;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            multiplier += Time.deltaTime * inputForce;
+        }
+        else
+        {
+            multiplier -= Time.deltaTime * inputForce;
+        }
+
+        multiplier = Mathf.Clamp(multiplier, 0, 1);
+
+        vfx.SetFloat("Multiplier", multiplier);
+    }
+}
